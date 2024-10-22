4efada368182c5aaf50a9237df7bf79d8da3f2c9
Sims309 <wmandaba.s@gmail.com>
2024-10-22 17:05:37 +0200

    composant pakémon créé

---
+++ src/Premier.tsx
@@ -0,0 +1,12 @@
+ 
+export default function Premier(){
+    return
+     <section>
+        <div>
+    <h1>Helllo World</h1>
+    <h2>Premier</h2>
+    <img src="" alt=""/>
+    <p>Pokémon</p>
+    </div>
+  </section>
+}
---
+++ src/components.tsx/PokemonCard.tsx
@@ -0,0 +1,14 @@
+function PokemonCard(){
+    return ( 
+        <div>
+            <h1>Helllo World pokemonCard</h1>
+            
+            <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+                <figcaption>Bulbasaur</figcaption>
+            </figure>
+        </div>
+        );
+}
+
+export default PokemonCard;
