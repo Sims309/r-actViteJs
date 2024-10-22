e01fc4c8919373b9a90a54dd9162256b9a09e057
Sims309 <wmandaba.s@gmail.com>
2024-10-22 18:37:32 +0200

    branche components.2 dynamisation de la liste

--- src/components.tsx/PokemonCard.tsx
+++ src/components.tsx/PokemonCard.tsx
@@ -1,14 +1,29 @@
 function PokemonCard(){
+    const pokemon = pokemonList[0].name;
+    const image = pokemonList[0].imgSrc
+
     return ( 
         <div>
             <h1>Helllo World pokemonCard</h1>
             
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-                <figcaption>Bulbasaur</figcaption>
+                <img src={image} alt={pokemon} />
+                <figcaption>{pokemon}</figcaption>
             </figure>
         </div>
         );
 }
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+    },
+  
+    {
+      name: "mew",
+    },
+  ];
+
 
 export default PokemonCard;
