524fa3f04d8a1aaa930793f987268cb882dda867
Sims309 <wmandaba.s@gmail.com>
2024-10-23 09:44:49 +0200

    ajouter liste PokemonList

--- src/components.tsx/PokemonCard.tsx
+++ src/components.tsx/PokemonCard.tsx
@@ -1,28 +1,32 @@
-function PokemonCard(){
-    const pokemon = pokemonList[0].name;
-    const image = pokemonList[0].imgSrc
-
+  
+   function PokemonCard () {
+      const pokemon = pokemonList[0].name;
+      const image = pokemonList[0].imgSrc;
+      // const pokemon = pokemonList[1].name;
+      // const image = pokemonList[1].imgSrc;
+      
     return ( 
         <div>
-            <h1>Helllo World pokemonCard</h1>
-            
-            <figure>
+            {image? 
+              <figure>
                 <img src={image} alt={pokemon} />
-                <figcaption>{pokemon}</figcaption>
-            </figure>
+                <figcaption>{pokemon}</figcaption>               
+            </figure>:<p>???</p>
+            }
         </div>
         );
 }
 const pokemonList = [
     {
       name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+       imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 
     },
   
     {
       name: "mew",
-    },
+    //   imgSrc:"???",
+     },
   ];
 
 
